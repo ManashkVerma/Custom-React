cc57f1001b53bdadf313fe8b71aa1f1238dea35d
Manash01 <156940539+ManashkVerma@users.noreply.github.com>
2025-06-19 20:43:37 +0530

    Update README and customRender function for clarity and structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Custom-React
+# Custom React (Version 1)
+
+This is the first version of a simple custom React-like renderer. It demonstrates how to render a basic element to the DOM using plain JavaScript.
--- customReact.js
+++ customReact.js
@@ -1,10 +1,21 @@
 function customRender(reactElement, container){
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
+
+    for(const prop in reactElement.props){
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+
+    container.appendChild(domElement)
+    
+    /* VERSION - 1
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
     domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
 
     container.appendChild(domElement)
+    */
 }
 
 const reactElement = {
